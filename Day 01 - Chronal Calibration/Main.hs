fa24238d4a141684a66451b72e38e81e97185167
Alexandre Perrin <alex@kaworu.ch>
2018-12-17 13:22:00 +0100

    Day 1: Simpler implementation

--- Day 01 - Chronal Calibration/Main.hs
+++ Day 01 - Chronal Calibration/Main.hs
@@ -1,6 +1,6 @@
 module Main (main) where
 
-import Data.Set
+import Data.Set hiding (foldl)
 import Text.Printf (printf)
 
 import Text.ParserCombinators.Parsec
@@ -11,112 +11,53 @@ import qualified Text.ParserCombinators.Parsec.Token as Token
 type Freq = Int
 
 -- | A change in the device's frequency.
-data Drift = Inc Freq | Dec Freq
-    deriving (Show)
+type Drift = Int
 
--- | Calibrate the device's frequency with a given change.
+-- | Adjust the device's frequency with a given change.
 --
--- >>> 0 `calibrate` (Inc 6)
+-- >>> 0 `adjust` 6
 -- 6
--- >>> 6 `calibrate` (Dec 1)
+-- >>> 6 `adjust` (-1)
 -- 5
-calibrate :: Freq -> Drift -> Freq
-calibrate freq (Inc delta) = freq + delta
-calibrate freq (Dec delta) = freq - delta
-
--- | The resulting frequency after all the changes have been applied.
-data FinalFreq = Measuring | Measured Freq
-    deriving (Show)
-
--- | Measure the resulting frequency after all the changes have been applied.
---
--- >>> Measuring `measure` 42
--- Measured 42
--- >>> (Measured 42) `measure` 9001
--- Measured 42
-measure :: FinalFreq -> Freq -> FinalFreq
-measure (Measured fin) _ = Measured fin
-measure Measuring fin = Measured fin
-
--- | The first frequency reached twice.
-data TwiceFreq = Empty | Searching (Set Freq) | Found Freq
-    deriving (Show)
-
--- | Look for the first frequency reached twice.
---
--- >>> Empty `search` 42
--- Searching (fromList [42])
--- >>> (Searching $ fromList [42]) `search` 9001
--- Searching (fromList [42,9001])
--- >>> (Searching $ fromList [42, 9001]) `search` 42
--- Found 42
--- >>> (Found 42) `search` 0
--- Found 42
-search :: TwiceFreq -> Freq -> TwiceFreq
-search (Found f) _ = Found f
-search (Searching s) f
-  | f `member` s = Found f
-  | otherwise    = Searching (f `insert` s)
-search Empty f = Searching (singleton f)
-
--- | Given a initial frequency, a complete list of changes, and a rest of list
--- of changes, measure the resulting frequency after all the changes have been
--- applied and find the first frequency reached twice.
-compute :: FinalFreq -> TwiceFreq -> Freq -> [Drift] -> [Drift] -> (Freq, Freq)
-compute (Measured ff) (Found t2) _ _ _ = (ff, t2)
-compute fin twice f ds [] = compute fin' twice f ds ds
-    where fin' = fin `measure` f
-compute fin twice f ds (x:xs) = compute fin twice' f' ds xs
-    where twice' = search twice f
-          f' = f `calibrate` x
-
--- | Given a initial frequency and a list of changes, measure the resulting
--- frequency after all the changes have been applied and find the first
--- frequency reached twice.
---
--- >>> compute' Measuring Empty initialFreq [Inc 1, Dec 2, Inc 3, Inc 1]
--- (3,2)
-compute' :: FinalFreq -> TwiceFreq -> Freq -> [Drift] -> (Freq, Freq)
-compute' fin twice f xs = compute fin twice f xs xs
+adjust :: Freq -> Drift -> Freq
+adjust = (+)
 
 -- | The device's initial frequency.
-initialFreq :: Freq
 initialFreq = 0
 
--- | The measured final device's frequency given a string representation of a
+-- | The measured final device's frequency given the initial frequency and a
 -- sequence of changes.
 --
--- >>> part1 "+1, -2, +3, +1"
--- Right 3
--- >>> part1 "+1, +1, +1"
--- Right 3
--- >>> part1 "+1, +1, -2"
--- Right 0
--- >>> part1 "-1, -2, -3"
--- Right (-6)
-part1 :: String -> Either ParseError Freq
-part1 input = either Left (Right . part1') parsed
-    where part1' = fst . compute' Measuring (Found 0) initialFreq
-          parsed = parse drifts "" input
+-- >>> calibrate 0 [+1, -2, +3, +1]
+-- 3
+-- >>> calibrate 0 [+1, +1, +1]
+-- 3
+-- >>> calibrate 0 [+1, +1, -2]
+-- 0
+-- >>> calibrate 0 [-1, -2, -3]
+-- -6
+calibrate :: Freq -> [Drift] -> Freq
+calibrate = foldl (adjust)
 
-
--- | The first device's frequency reached twice given a string representation
--- of a sequence of changes to cycle through.
+-- | The first device's frequency reached twice given the initial frequency and
+-- a sequence of changes to cycle through.
 --
--- >>> part2 "+1, -2, +3, +1"
+-- >>> calibrate' 0 [+1, -2, +3, +1]
 -- Right 2
--- >>> part2 "+1, -1"
+-- >>> calibrate' 0 [+1, -1]
 -- Right 0
--- >>> part2 "+3, +3, +4, -2, -4"
+-- >>> calibrate' 0 [+3, +3, +4, -2, -4]
 -- Right 10
--- >>> part2 "-6, +3, +8, +5, -6"
+-- >>> calibrate' 0 [-6, +3, +8, +5, -6]
 -- Right 5
--- >>> part2 "+7, +7, -2, -7, -4"
+-- >>> calibrate' 0 [+7, +7, -2, -7, -4]
 -- Right 14
-part2 :: String -> Either ParseError Freq
-part2 input = either Left (Right . part2') parsed
-    where part2' = snd . compute' (Measured 0) Empty initialFreq
-          parsed = parse drifts "" input
+calibrate' :: Freq -> [Drift] -> Freq
+calibrate' f ds = dup empty freqs
+    where freqs = scanl (adjust) f $ cycle ds
+          dup s (x:xs)
+              | x `member` s = x
+              | otherwise    = dup (x `insert` s) xs
 
 -- | Display the answers given the resulting frequency and the first frequency
 -- reached twice.
@@ -129,28 +70,30 @@ main = do
     input <- getContents
     case parse drifts "" input of
       Left err -> print err >> fail "parse error"
-      Right ds -> answer (fst freqs) (snd freqs)
-          where freqs = compute' Measuring Empty initialFreq ds
+      Right xs -> answer calibrated calibrated'
+          where calibrated  = calibrate  initialFreq xs
+                calibrated' = calibrate' initialFreq xs
 
 -- | Parse a Drift value.
 --
 -- >>> parse drift "" "+3"
--- Right (Inc 3)
+-- Right 3
 -- >>> parse drift "" "-6"
--- Right (Dec 6)
+-- Right (-6)
 -- >>> parse drift "" "+42  "
--- Right (Inc 42)
+-- Right 42
 drift :: Parser Drift
 drift = do
-    sign <- oneOf "+-"
-    num  <- many1 digit
-    _    <- spaces
-    return $ (if sign == '+' then Inc else Dec) (read num)
+    sign   <- oneOf "+-"
+    digits <- many1 digit
+    _      <- spaces
+    let num = read digits
+     in return $ (if sign == '+' then num else (- num))
 
 -- | Parse a sequence of Drift.
 --
 -- >>> parse drifts "" "+1, -2, +3, +1"
--- Right [Inc 1,Dec 2,Inc 3,Inc 1]
+-- Right [1,-2,3,1]
 drifts :: Parser [Drift]
 drifts = (sepBy drift sep) <* eof
     where sep = optional (char ',') >> spaces
