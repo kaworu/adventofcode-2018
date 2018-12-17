2722b8afd9f0f1e2f1ad419643fa6dc170197d0e
Alexandre Perrin <alex@kaworu.ch>
2018-12-17 21:30:02 +0100

    Day 2: Inventory Management System (part 2)

--- Day 02 - Inventory Management System/Main.hs
+++ Day 02 - Inventory Management System/Main.hs
@@ -26,14 +26,65 @@ type BoxID = [Char]
 -- [('a',1),('b',1),('c',1),('d',1),('e',2)]
 -- >>> freq "ababab"
 -- [('a',3),('b',3)]
--- >>> freq [1, 2, 2, 3, 3, 3]
--- [(1,1),(2,2),(3,3)]
+-- >>> freq []
+-- []
 freq :: Ord a => [a] -> [(a, Int)]
 freq = map (\xs -> (head xs, length xs)) . group . sort
 
--- | Display the box IDs list checksum.
-answer :: Int -> IO ()
-answer = printf "The checksum is %d.\n"
+-- | The box IDs list checksum.
+--
+-- >>> checksum ["abcdef", "bababc", "abbcde", "abcccd", "aabcdd", "abcdee", "ababab"]
+-- 12
+-- >>> checksum []
+-- 0
+checksum :: [BoxID] -> Int
+checksum xs = (length twos) * (length threes)
+    where twos   = filter (any (==2)) freqs
+          threes = filter (any (==3)) freqs
+          freqs  = map (map snd . freq) xs
+
+-- | The hamming distance of two lists.
+-- | see https://stackoverflow.com/a/52277553.
+--
+-- >>> distance "abcde" "axcye"
+-- 2
+-- >>> distance "fghij" "fguij"
+-- 1
+distance :: Eq a => [a] -> [a] -> Int
+distance xs ys = length $ filter id $ zipWith (/=) xs ys
+
+-- | All uniques pairs of a list.
+-- | see https://stackoverflow.com/a/34045121
+--
+-- >>> pairs [1, 2, 3, 4]
+-- [(1,2),(1,3),(1,4),(2,3),(2,4),(3,4)]
+-- >>> pairs []
+-- []
+-- >>> pairs ["alone"]
+-- []
+-- >>> pairs ["fst", "snd"]
+-- [("fst","snd")]
+pairs :: [a] -> [(a, a)]
+pairs l = [(x, y) | (x:ys) <- tails l, y <- ys]
+
+-- | The pair of BoxID which differ by exactly one character at the same
+-- position.
+--
+-- >>> prototypes ["abcde", "fghij", "klmno", "pqrst", "fguij", "axcye", "wvxyz"]
+-- Just ("fghij","fguij")
+-- >>> prototypes ["abcde", "axcye"]
+-- Nothing
+-- >>> prototypes []
+-- Nothing
+prototypes :: [BoxID] -> Maybe (BoxID, BoxID)
+prototypes = find matchingDistance . pairs
+    where matchingDistance (x, y) = (distance x y) == 1
+
+-- | Display the box IDs list checksum and the common letters between the two
+-- correct box IDs.
+answer :: Int -> Maybe [Char] -> IO ()
+answer i Nothing   = printf "The checksum is %d, there are no correct box IDs.\n" i
+answer i (Just xs) = printf "The checksum is %d, \"%s\" are the common letters between the two correct box IDs.\n" i xs
 
 -- | Compute and display the rudimentary checksum of the given list of box IDs.
 main :: IO ()
@@ -41,10 +92,9 @@ main = do
     input <- getContents
     case parse boxIds "" input of
       Left err -> print err >> fail "parse error"
-      Right xs -> answer ((length twos) * (length threes))
-          where twos   = filter (any (==2)) freqs
-                threes = filter (any (==3)) freqs
-                freqs  = map (map snd . freq) xs
+      Right xs -> answer (checksum xs) (common $ prototypes xs)
+          where common Nothing       = Nothing
+                common (Just (a, b)) = Just (a `intersect` b)
 
 -- | Parse a BoxID.
 --
@@ -52,9 +102,9 @@ main = do
 -- Right "foobar"
 boxId :: Parser BoxID
 boxId = do
-    id <- many1 lower
-    _  <- spaces
-    return id
+    s <- many1 lower
+    _ <- spaces
+    return s
 
 -- | Parse a sequence of BoxID.
 --
