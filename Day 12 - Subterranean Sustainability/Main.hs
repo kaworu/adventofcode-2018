60010adac6a811ba32b7e8022ff1d0e0568fad8a
Alexandre Perrin <alex@kaworu.ch>
2019-03-21 22:00:14 +0100

    Day 12: Subterranean Sustainability (part 2)

--- Day 12 - Subterranean Sustainability/Main.hs
+++ Day 12 - Subterranean Sustainability/Main.hs
@@ -9,12 +9,16 @@ import qualified Data.Map.Strict as Map
 data Pot = Empty | Plant
     deriving (Eq, Ord)
 
--- | The pattern part of a note found on a nearby table.
+-- | The pattern part of a note.
 data Pattern = Pattern Pot Pot Pot Pot Pot
     deriving (Eq, Ord)
 
--- | The tunnel with the first pot's number, the pots, and the notes.
-data Tunnel = Tunnel Int [Pot] (Map Pattern Pot)
+-- | The notes found on the nearby table.
+type Notes = Map Pattern Pot
+
+-- | The tunnel with its generation, first pot number, pots, and notes.
+data Garden = Garden { num :: Int, pots :: [Pot], notes :: Notes }
+    deriving (Show)
 
 -- | Display a pot as in README.md
 --
@@ -33,9 +37,32 @@ instance Show Pot where
 instance Show Pattern where
     show (Pattern ll l c r rr) = concatMap show [ll, l, c, r, rr]
 
--- | Display the first pot number and the state of the tunnel.
-instance Show Tunnel where
-    show (Tunnel i pots _) = printf "%d\n%s" i (concatMap show pots)
+-- | The given pots without the leading Empty ones.
+--
+-- >>> lstrip [Empty, Pot, Empty]
+-- [#,.]
+-- >>> lstrip [Pot, Empty, Empty]
+-- [#,.,.]
+-- >>> lstrip [Empty, Empty, Empty]
+-- []
+-- >>> lstrip []
+-- []
+lstrip :: [Pot] -> [Pot]
+lstrip (Empty : xs) = lstrip xs
+lstrip xs = xs
+
+-- | The given pots without the trailing Empty ones.
+--
+-- >>> rstrip [Empty, Pot, Empty]
+-- [.,#]
+-- >>> rstrip [Pot, Empty, Empty]
+-- [#]
+-- >>> rstrip [Empty, Empty, Empty]
+-- []
+-- >>> rstrip []
+-- []
+rstrip :: [Pot] -> [Pot]
+rstrip = reverse . lstrip . reverse
 
 -- | An incomplete Pattern missing only the rightmost pot, build from the
 -- previous incomplete pattern and the pot two spots to the right of the
@@ -44,41 +71,62 @@ shiftl :: (Pot -> Pattern) -> Pot -> (Pot -> Pattern)
 shiftl f p = Pattern l c r rr
     where (Pattern _ l c r rr) = f p
 
--- | The same Tunnel without the leading and trailing empty pots accounted for.
-simplify :: Tunnel -> Tunnel
-simplify (Tunnel i (Empty : xs) m) = simplify $ Tunnel (i + 1) xs m
-simplify (Tunnel i pots m) = Tunnel i (reverse $ chomp rev) m
-    where rev = reverse pots
-          chomp (Empty : xs) = xs
-          chomp xs = xs
+-- | The same Garden without the leading and trailing empty pots accounted for
+-- in its state.
+simplify :: Garden -> Garden
+simplify (Garden n ps m) = Garden (n + nlstripped) stripped m
+    where nlstripped = length ps - length lstripped
+          lstripped = lstrip ps
+          stripped = rstrip lstripped
 
--- | The next generation of the given tunnel.
-next :: Tunnel -> Tunnel
-next (Tunnel i pots m) = simplify $ Tunnel (i - 2) (ev p0 pots) m
+-- | The next generation of the given garden.
+next :: Garden -> Garden
+next (Garden n ps m) = simplify $ Garden (n - 2) (evo p0 ps) m
     where p0 = Pattern Empty Empty Empty Empty
-          ev f (x : xs) = (m ! f x) : ev (shiftl f x) xs
-          ev f [] = fin f (replicate 4 Empty)
+          evo f (x : xs) = (m ! f x) : evo (shiftl f x) xs
+          evo f [] = fin f (replicate 4 Empty)
           fin f (x : xs) = (m ! f x) : fin (shiftl f x) xs
           fin _ [] = []
 
+-- | The given garden after i generation.
+grow :: Int -> Garden -> Garden
+grow 0 g = g
+grow i g = grow' (i - 1) g (next g)
+
+-- | Memoized version of grow detecting when the state stabilize.
+--
+-- This implementation has the limitation of short-cutting only when the state
+-- stabilize by shifting itself. We could detect loops by using a Map,
+-- effectively memoizing all the state already seen, but remembering only the
+-- last state is enough for this challenge.
+grow' :: Int -> Garden -> Garden -> Garden
+grow' 0 _ g = g
+grow' i prev g@(Garden n ps m)
+  | pots prev == ps = Garden (n + i * (n - num prev)) ps m
+  | otherwise = grow' (i - 1) g (next g)
+
 -- | The sum of the number of all pots which contain a plant.
-plants :: Tunnel -> Int
-plants (Tunnel i pots _) = sum $ map snd $ filter planted $ zip pots [i..]
+plants :: Garden -> Int
+plants (Garden n ps _) = sum $ map snd $ filter planted $ zip ps [n..]
     where planted (p, _) = p == Plant
 
 -- | Display the sum of the numbers of all pots which contain a plant after 20
 -- generations.
-answer :: Tunnel -> IO ()
-answer = printf "The sum of the numbers of all pots which contain a plant after 20 generations is %d.\n" . plants
+answer :: Garden -> Garden -> IO ()
+answer g20 g50b = do
+    printf "The sum of the numbers of all pots which contain a plant after 20 generations is %d,\n" (plants g20)
+    printf "and after fifty billion generations it is %d.\n" (plants g50b)
 
 -- | Compute and display the sum of the numbers of all pots which contain a
 -- plant after 20 generations.
 main :: IO ()
 main = do
     input <- getContents
-    case parse tunnel "" input of
+    case parse garden "" input of
       Left err -> error (show err)
-      Right  t -> answer $ last $ take 21 $ iterate next t
+      Right g0 -> answer g20 g50b
+          where g20  = grow 20 g0
+                g50b = grow (50000000000 - 20) g20
 
 -- | Parse '.' as an Empty pot and '#' as a Plant.
 --
@@ -94,12 +142,12 @@ pot = do
     c <- oneOf ".#"
     return (if c == '.' then Empty else Plant)
 
--- | Parse the tunnel's initial state
+-- | Parse the garden initial state
 --
--- >>> parse state "" "initial state: #..#.#..##......###...###"
+-- >>> parse initialState "" "initial state: #..#.#..##......###...###"
 -- Right [#,.,.,#,.,#,.,.,#,#,.,.,.,.,.,.,#,#,#,.,.,.,#,#,#]
-state :: Parser [Pot]
-state = spaces >> string "initial state: " >> many1 pot
+initialState :: Parser [Pot]
+initialState = spaces >> string "initial state: " >> many1 pot
 
 -- | Parse a note found on the nearby table.
 --
@@ -120,9 +168,9 @@ note = do
      n  <- spaces >> string "=>" >> spaces >> pot <* spaces
      return (Pattern ll l c r rr, n)
 
--- | Parse the tunnel's state and the notes.
-tunnel :: Parser Tunnel
-tunnel = do
-    s  <- state
+-- | Parse the garden state and the notes.
+garden :: Parser Garden
+garden = do
+    st <- initialState
     xs <- many1 note
-    return $ Tunnel 0 s (Map.fromList xs)
+    return $ Garden { num = 0, pots = st, notes = Map.fromList xs }
---
+++ Day 12 - Subterranean Sustainability/answer.part2.md
@@ -0,0 +1,1 @@
+Your puzzle answer was `4550000002111`.
