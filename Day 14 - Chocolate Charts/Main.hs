0762398a53b72b5952a455f2664840ba3ea1f55c
Alexandre Perrin <alex@kaworu.ch>
2019-06-06 23:23:51 +0200

    Day 14: Chocolate Charts (part 2)
    Rip-off mstksg elegant solution's [1]. That was eye opening, still a lot
    to learn.
    
    [1]: https://www.reddit.com/r/adventofcode/comments/a61ojp/2018_day_14_solutions/ebr5c0q?utm_source=share&utm_medium=web2x

--- Day 14 - Chocolate Charts/Main.hs
+++ Day 14 - Chocolate Charts/Main.hs
@@ -1,93 +1,82 @@
 module Main (main) where
 
-import Data.IntMap (IntMap, (!))
+import Data.Char
+import Data.List
 import Text.ParserCombinators.Parsec
 import Text.Printf (printf)
-import qualified Data.IntMap.Strict as IntMap
+import qualified Data.Sequence as Seq
 
 -- A recipe score.
 type Score = Int
 
--- | The kitchen's scoreboard for all cooked recipes.
-type ScoreBoard = IntMap Score
-
--- | The current recipe of both elves.
-type Elves = (Int, Int)
-
--- | The whole laboratory.
+-- | The scoreboard.
 --
--- Keeping track of the length is important for performance reasons as
--- IntMap.size is O(n).
-data Kitchen = Kitchen { elves :: Elves, len :: Int, scores :: ScoreBoard }
-    deriving (Show)
-
--- | The initial scoreboard.
---
--- >>> board
--- Kitchen {elves = (0,1), len = 2, scores = fromList [(0,3),(1,7)]}
-board :: Kitchen
-board = Kitchen (0, 1) 2 $ IntMap.fromList [(0, 3), (1, 7)]
-
--- | Add the recipe to the kitchen's scoreboard.
---
--- >>> append 10 board
--- Kitchen {elves = (0,1), len = 4, scores = fromList [(0,3),(1,7),(2,1),(3,0)]}
-append :: Score -> Kitchen -> Kitchen
-append x k@Kitchen { len = l, scores = s }
-  | x >= 10 = append (x `mod` 10) $ append (x `div` 10) k
-  | otherwise = Kitchen (elves k) (l + 1) (IntMap.insert l x s)
-
--- | Cook a round of recipe.
---
--- >>> cook board
--- Kitchen {elves = (0,1), len = 4, scores = fromList [(0,3),(1,7),(2,1),(3,0)]}
--- >>> cook $ cook board
--- Kitchen {elves = (4,3), len = 6, scores = fromList [(0,3),(1,7),(2,1),(3,0),(4,1),(5,0)]}
--- >>> iterate cook board !! 3
--- Kitchen {elves = (6,4), len = 7, scores = fromList [(0,3),(1,7),(2,1),(3,0),(4,1),(5,0),(6,1)]}
-cook :: Kitchen -> Kitchen
-cook k@Kitchen { elves = (a, b), scores = s } = Kitchen (a', b') l' s'
-    where a' = (a + sa + 1) `mod` l'
-          b' = (b + sb + 1) `mod` l'
-          Kitchen { len = l', scores = s' } = append (sa + sb) k
-          (sa, sb) = (s ! a, s ! b)
+-- >>> take 20 board
+-- [3,7,1,0,1,0,1,2,4,5,1,5,8,9,1,6,7,7,9,2]
+board :: [Score]
+board = 3 : 7 : next (0, 1) (Seq.fromList [3, 7]) []
+    where next (a, b) full (x : xs) = x : next (a, b) full xs
+          next (a, b) full [] = next (a', b') full' new
+            where new = if s < 10 then [s] else [s `div` 10, s `mod` 10]
+                  s = sa + sb
+                  sa = Seq.index full a
+                  sb = Seq.index full b
+                  full' = full <> Seq.fromList new
+                  a' = (a + sa + 1) `mod` length full'
+                  b' = (b + sb + 1) `mod` length full'
 
 -- | The scores of the i recipes after n recipes.
 --
--- >>> ultimate 10 5 board
+-- >>> iafter 10 5
 -- [0,1,2,4,5,1,5,8,9,1]
--- >>> ultimate 10 18 board
+-- >>> iafter 10 18
 -- [9,2,5,1,0,7,1,0,8,5]
--- >>> ultimate 10 2018 board
+-- >>> iafter 10 2018
 -- [5,9,4,1,4,2,9,8,8,2]
-ultimate :: Int -> Int -> Kitchen -> [Int]
-ultimate i n k@Kitchen { len = l, scores = s }
-  | l >= (n + i) = take i $ drop n $ IntMap.elems s
-  | otherwise    = ultimate i n $ cook k
+iafter :: Int -> Int -> [Score]
+iafter i n = take i $ drop n board
 
--- | Display the 10 recipes immediately after a given number of recipes.
-answer :: Int -> [Score] -> IO ()
-answer n xs = printf fmt n (concat $ map show xs)
-    where fmt = "the scores of the ten recipes immediately after %d are %s.\n"
+-- | The number of recipes after which the given score pattern appears.
+--
+-- >>> ultimate [5, 1, 5, 8, 9]
+-- 9
+-- >>> ultimate [0, 1, 2, 4, 5]
+-- 5
+-- >>> ultimate [9, 2, 5, 1, 0]
+-- 18
+-- >>> ultimate [5, 9, 4, 1, 4]
+-- 2018
+ultimate :: [Int] -> Int
+ultimate xs = length $ takeWhile (not . (xs `isPrefixOf`)) $ tails board
+
+-- | Display the 10 recipes immediately after a given number of recipes, and
+-- the number of recipes after which the score pattern appears.
+answer :: String -> [Score] -> Int -> IO ()
+answer desc xs n = do
+    printf "the scores of the ten recipes immediately after %d recipes are %s,\n" (read desc :: Int) (concat $ map show xs)
+    printf "and %s first appears after %d recipes.\n" desc n
 
 -- | Compute and display the 10 recipes immediately after a given number of
--- recipes.
+-- recipes, and the number of recipes after which the score pattern appears.
 main :: IO ()
 main = do
     input <- getContents
-    case parse number "" input of
+    case parse digits "" input of
       Left err -> fail (show err)
-      Right n  -> answer n (ultimate 10 n board)
+      Right desc -> answer desc (iafter 10 n) (ultimate $ map digitToInt desc)
+          where n = read desc
 
 -- | Parse the recipes count.
 --
--- >>> parse number "" "57"
--- Right 57
--- >>> parse number "" "39"
--- Right 39
--- >>> parse number "" "71"
--- Right 71
-number :: Parser Int
-number = do
-    digits <- spaces >> many1 digit
-    return (read digits)
+-- >>> parse digits "" "57"
+-- Right "57"
+-- >>> parse digits "" "039"
+-- Right "039"
+-- >>> parse digits "" "71"
+-- Right "71"
+-- >>> parse digits "" "NOPE"
+-- Left (line 1, column 1):
+-- unexpected "N"
+-- expecting white space or digit
+digits :: Parser String
+digits = spaces >> many1 digit
